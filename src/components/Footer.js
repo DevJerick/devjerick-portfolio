b564383a496bde5abe3251e7da240a31439d1fb2
jerick sejas <it.jericksejas@gmail.com>
2022-12-23 16:41:59 +0800

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2021 DevJerick.</p>
+    </footer>
+  );
+};
+
+export default Footer;
